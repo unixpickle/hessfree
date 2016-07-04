f8f10d54c4d1c0966eea88c4acf857c88245641d
Alex Nichol <unixpickle@gmail.com>
2016-07-04 10:23:18 -0400

    benchmark QuadApprox

--- learner_test.go
+++ learner_test.go
@@ -53,6 +53,44 @@ func TestDampedNeuralLearner(t *testing.T) {
 	testLearner(t, learner, sampleSet)
 }
 
+func BenchmarkDampedNeuralLearnerQuadHessian(b *testing.B) {
+	network := neuralnet.Network{
+		&neuralnet.DenseLayer{
+			InputCount:  100,
+			OutputCount: 1000,
+		},
+		neuralnet.Sigmoid{},
+		&neuralnet.DenseLayer{
+			InputCount:  1000,
+			OutputCount: 100,
+		},
+	}
+	network.Randomize()
+
+	learner := &DampingLearner{
+		WrappedLearner: &NeuralNetLearner{
+			Layers:         network,
+			Output:         nil,
+			Cost:           neuralnet.SigmoidCECost{},
+			MaxSubBatch:    10,
+			MaxConcurrency: 1,
+		},
+		DampingCoeff: 1,
+	}
+
+	var inputs []linalg.Vector
+	for i := 0; i < 50; i++ {
+		vec := make(linalg.Vector, 100)
+		for i := range vec {
+			vec[i] = rand.Float64()
+		}
+		inputs = append(inputs, vec)
+	}
+	sampleSet := neuralnet.VectorSampleSet(inputs, inputs)
+
+	benchLearnerQuadApprox(b, learner, sampleSet)
+}
+
 func testLearner(t *testing.T, l Learner, s sgd.SampleSet) {
 	objective := l.MakeObjective()
 
@@ -90,5 +128,23 @@ GradLoop:
 				break GradLoop
 			}
 		}
+	}
+}
+
+func benchLearnerQuadApprox(b *testing.B, l Learner, s sgd.SampleSet) {
+	destination := ConstParamDelta{}
+	for _, v := range l.Parameters() {
+		vec := make(linalg.Vector, len(v.Vector))
+		destination[v] = vec
+		for i := range vec {
+			vec[i] = rand.NormFloat64() * learnerTestOffset
+		}
+	}
+
+	obj := l.MakeObjective()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		obj.QuadHessian(destination, s)
 	}
 }
